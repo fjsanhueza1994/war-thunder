d56777bc60ccb03bccb2ad86ba5366fda476ffeb
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-15 20:28:31 -0300

    ^ Add a PilotDao repository.

---
+++ ms-pilots/src/main/java/com/example/ms_pilots/models/dao/PilotDao.java
@@ -0,0 +1,10 @@
+package com.example.ms_pilots.models.dao;
+
+import com.example.ms_pilots.models.entity.Pilot;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PilotDao extends CrudRepository<Pilot, Long> {
+
+}
