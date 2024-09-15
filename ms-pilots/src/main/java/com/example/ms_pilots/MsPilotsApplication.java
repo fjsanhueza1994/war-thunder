7f214769060b7cc10bd837d235865345a5a3fc69
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-15 20:15:17 -0300

    + First Warthunder project commit.

---
+++ ms-pilots/src/main/java/com/example/ms_pilots/MsPilotsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_pilots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPilotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPilotsApplication.class, args);
+	}
+
+}
---
+++ ms-pilots/src/test/java/com/example/ms_pilots/MsPilotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_pilots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPilotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
