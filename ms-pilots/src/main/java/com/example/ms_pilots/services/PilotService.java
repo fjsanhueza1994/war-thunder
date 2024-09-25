94d4a01750f97977974720e01b2c50f76d88d205
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-25 13:25:23 -0300

    ^ Create pilot service.

---
+++ ms-pilots/src/main/java/com/example/ms_pilots/services/PilotService.java
@@ -0,0 +1,14 @@
+package com.example.ms_pilots.services;
+
+import com.example.ms_pilots.models.entity.Pilot;
+
+import java.util.List;
+
+public interface PilotService {
+
+    public List<Pilot> findAll();
+    public Pilot findById(Long id);
+    public Pilot save(Pilot pilot);
+//    public Pilot update(Pilot pilot, Long id);
+    public void deleteById(Long id);
+}
