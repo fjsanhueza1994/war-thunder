4eb56ca4229a5d7737e710f7c7f3d5248154d8da
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-25 13:23:33 -0300

    ^ Add table name, column db name and id generations strategy.

--- ms-pilots/src/main/java/com/example/ms_pilots/models/entity/Pilot.java
+++ ms-pilots/src/main/java/com/example/ms_pilots/models/entity/Pilot.java
@@ -1,20 +1,23 @@
 package com.example.ms_pilots.models.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "pilot")
 public class Pilot {
 
     @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String name;
+    @Column(name = "last_name")
     private String lastName;
     private String dni;
     private String address;
