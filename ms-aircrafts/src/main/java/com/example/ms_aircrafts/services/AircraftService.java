eb71e3e71fc3e7e6743c8bf8d295fb1a5d93459c
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-26 15:35:27 -0300

    + Create aircraft service.

---
+++ ms-aircrafts/src/main/java/com/example/ms_aircrafts/services/AircraftService.java
@@ -0,0 +1,14 @@
+package com.example.ms_aircrafts.services;
+
+import java.util.List;
+
+import com.example.ms_aircrafts.models.entity.Aircraft;
+
+public interface AircraftService {
+
+    public List<Aircraft> findAll();
+    public Aircraft findById(Long id);
+    public Aircraft save(Aircraft aircraft);
+    public void deleteById(Long id);
+    
+}
