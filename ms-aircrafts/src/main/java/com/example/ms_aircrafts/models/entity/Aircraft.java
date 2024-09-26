208c9f0900c673b8257accd66d39f57c0ba5284e
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-26 14:36:17 -0300

    ^ Add Aircraft entity.

---
+++ ms-aircrafts/src/main/java/com/example/ms_aircrafts/models/entity/Aircraft.java
@@ -0,0 +1,24 @@
+package com.example.ms_aircrafts.models.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "aircraft")
+public class Aircraft {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+    private String model;
+    private String manufacturer;
+    private int year;
+    private int seats;
+
+}
