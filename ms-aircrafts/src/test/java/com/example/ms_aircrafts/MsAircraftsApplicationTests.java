0f0f7c0b789e7e6365846d823ef57a39dfb10684
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-26 13:52:15 -0300

    + Add new aircrafts service.

---
+++ ms-aircrafts/src/test/java/com/example/ms_aircrafts/MsAircraftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_aircrafts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAircraftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
