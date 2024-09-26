5f4410ce709da428f21ad97723a8004a118da53c
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-26 15:46:05 -0300

    + Implement Pilot Service.

---
+++ ms-aircrafts/src/main/java/com/example/ms_aircrafts/controller/AircraftServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.ms_aircrafts.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ms_aircrafts.models.dao.AircraftDao;
+import com.example.ms_aircrafts.models.entity.Aircraft;
+import com.example.ms_aircrafts.services.AircraftService;
+
+@Service
+public class AircraftServiceImpl implements AircraftService {
+
+    private final AircraftDao aircraftDao;
+
+    @Autowired
+    public AircraftServiceImpl(AircraftDao aircraftDao) {
+        this.aircraftDao = aircraftDao;
+    }
+
+    @Override
+    public List<Aircraft> findAll() {
+        return (List<Aircraft>) aircraftDao.findAll();
+    }
+
+    @Override
+    public Aircraft findById(Long id) {
+        return aircraftDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public Aircraft save(Aircraft aircraft) {
+        return aircraftDao.save(aircraft);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        aircraftDao.deleteById(id);
+    }
+    
+}
