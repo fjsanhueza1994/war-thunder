1f454fa4dfdbf7fb6dd479fbc7f188f3ce7a65f9
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-30 13:18:57 -0300

    + Create service pilotaircraft to make association between aircrafts and pilots.

---
+++ ms-pilotaircraft/src/main/java/com/example/ms_pilotaircraft/MsPilotaircraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_pilotaircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPilotaircraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPilotaircraftApplication.class, args);
+	}
+
+}
---
+++ ms-pilotaircraft/src/test/java/com/example/ms_pilotaircraft/MsPilotaircraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_pilotaircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPilotaircraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
