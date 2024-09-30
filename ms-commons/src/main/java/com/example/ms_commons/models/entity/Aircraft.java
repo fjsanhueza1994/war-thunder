175f85d75703f6203da3c0d3594cb5ac0e1894de
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-30 13:35:59 -0300

    ^ Add Entity Aircraft to commons service.

--- ms-commons/src/main/java/com/example/ms_commons/models/entity/Aircraft.java
+++ ms-commons/src/main/java/com/example/ms_commons/models/entity/Aircraft.java
@@ -1,4 +1,87 @@
 package com.example.ms_commons.models.entity;
 
-public class Aircraft {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+
+@Entity(name = "aircraft")
+public class Aircraft implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String model;
+    private String registration;
+    private String manufacturer;
+    @Column(name = "manufactured_year")
+    private int manufacturedYear;
+    private int seats;
+    @Column(name = "flying_hours")
+    private int flyingHours;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getManufacturedYear() {
+        return manufacturedYear;
+    }
+
+    public void setManufacturedYear(int manufacturedYear) {
+        this.manufacturedYear = manufacturedYear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getFlyingHours() {
+        return flyingHours;
+    }
+
+    public void setFlyingHours(int flyingHours) {
+        this.flyingHours = flyingHours;
+    }
+
+    @Serial
+    private static final long serialVersionUID = -1149937310139952161L;
 }
