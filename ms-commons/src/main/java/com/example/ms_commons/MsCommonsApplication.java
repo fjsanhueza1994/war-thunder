07f0132197b795e1d375746870a2363364677e14
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-09-30 11:39:07 -0300

    Revert "^ Add aircraft and pilot entities to the commons microservice."
    This reverts commit 9c70a14a8ce9c2f1ec76244604b3c9c77451e63a.

--- ms-commons/src/main/java/com/example/ms_commons/MsCommonsApplication.java
+++ ms-commons/src/main/java/com/example/ms_commons/MsCommonsApplication.java
@@ -1,11 +1,13 @@
 package com.example.ms_commons;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) // Viene configurado por defecto en SpringBootApplication
 public class MsCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCommonsApplication.class, args);
+    }
 
 }
