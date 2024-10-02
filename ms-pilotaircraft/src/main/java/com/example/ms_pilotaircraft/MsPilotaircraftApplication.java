2f403c850fc436848444e3ba7d811829d77e1b6d
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-10-02 18:15:20 -0300

    ^ Use of ms-commons library on ms-pilotaircraft service.

--- ms-pilotaircraft/src/main/java/com/example/ms_pilotaircraft/MsPilotaircraftApplication.java
+++ ms-pilotaircraft/src/main/java/com/example/ms_pilotaircraft/MsPilotaircraftApplication.java
@@ -2,8 +2,13 @@ package com.example.ms_pilotaircraft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+		"com.example.ms_commons.models.entity",
+		"com.example.ms_pilotaircraft.models.entity"
+})
 public class MsPilotaircraftApplication {
 
 	public static void main(String[] args) {
