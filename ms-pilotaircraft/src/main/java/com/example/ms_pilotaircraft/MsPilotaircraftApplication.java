82378aeb503825f265e34bd2e8321d23436e8a27
Francisco Sanhueza <fjsanhueza1994@gmail.com>
2024-10-03 19:12:51 -0300

    ^ Implement Consul as a name server.

--- ms-pilotaircraft/src/main/java/com/example/ms_pilotaircraft/MsPilotaircraftApplication.java
+++ ms-pilotaircraft/src/main/java/com/example/ms_pilotaircraft/MsPilotaircraftApplication.java
@@ -3,8 +3,10 @@ package com.example.ms_pilotaircraft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EntityScan(basePackages = {
 		"com.example.ms_commons.models.entity",
 		"com.example.ms_pilotaircraft.models.entity"
--- ms-pilots/src/main/java/com/example/ms_pilots/MsPilotsApplication.java
+++ ms-pilots/src/main/java/com/example/ms_pilots/MsPilotsApplication.java
@@ -3,8 +3,10 @@ package com.example.ms_pilots;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EntityScan(basePackages = "com.example.ms_commons.models.entity")
 public class MsPilotsApplication {
 
